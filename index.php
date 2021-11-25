1f0270fe108167180f886dfa9b51e0d035e5e06a
handyzulkarnain <68466944+handyzulkarnain@users.noreply.github.com>
2021-11-25 23:01:35 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php ?>
+
+<html>
+  <head>
+    <title>Transmart Smart Service</title>
+    <link rel="stylesheet" href="asset/styles/style.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  </head>
+
+  <body>
+    <header></header>
+    <main>
+      <div>
+        <h1>Transmart Monitoring & Controlling System</h1>
+      </div>
+      <section>
+        <div>
+          <h2>Pilih Cabang Transmart</h2>
+          <div class="container">
+            <div class="box card">
+              <img src="asset/img/transmart bandung.jpg"/>
+              <h3>Nama Branch: Transmart Buah Batu Bandung</h3>
+              <h3>Lokasi: Jalan Bojongsoang Raya No. 269, Cipagalo, Bojongsoang, Cipagalo, Bojongsoang, Bandung, Jawa Barat 40287</h3>
+              <h3>Tingkat Keramaian: Lorem Ipsum</h3>
+              <button type="button">
+                Pilih
+              </button>
+            </div>
+            <div class="box card">
+              <img src="asset/img/transmart bandung.jpg"/>
+              <h3>Nama Branch: Lorem Ipsum</h3>
+              <h3>Lokasi: Lorem Ipsum</h3>
+              <h3>Tingkat Keramaian: Lorem Ipsum</h3>
+              <button type="button">
+                Pilih
+              </button>
+            </div>
+            <div class="box card">
+              <img src="asset/img/transmart bandung.jpg"/>
+              <h3>Nama Branch: Lorem Ipsum</h3>
+              <h3>Lokasi: Lorem Ipsum</h3>
+              <h3>Tingkat Keramaian: Lorem Ipsum</h3>
+              <button type="button">
+                Pilih
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+    <footer></footer>
+  </body>
+</html>
